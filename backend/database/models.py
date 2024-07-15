a659bfed43ed907ad435c66cfc278f402d8e1eb0
blake2703 <dickersonb27@yahoo.com>
2024-07-14 17:39:44 -0700

    databse testing and setup

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, text, event
+
 
 db = SQLAlchemy()
 
@@ -8,7 +11,7 @@ class UserDev(db.Model):
     __tablename__ = "users_dev"
     
     # Define schema
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(64), nullable=False)
     last_name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
@@ -16,31 +19,44 @@ class UserDev(db.Model):
     
     @property
     def password(self):
-        """
-        Determines if the password is hashable
-        """
-        raise AttributeError("Password is not hashable")
+        raise AttributeError("Password is not readable")
     
     @password.setter
     def password(self, password):
-        """
-        Helper function to generate password before it reaches the database
-
-        Args:
-            password (str): Password inputted by user
-        """
         self.password_hash = generate_password_hash(password)
     
     def verify_password(self, password):
-        """
-        Verifies if the hashed password is equivalent to the non-hashed password
-
-        Args:
-            password (str): Password inputted by user
-
-        Returns:
-            bool: Determines if the passwords are equivalent
-        """
         return check_password_hash(self.password_hash, password)
+
+class Stock(db.Model):
+    __tablename__ = "stocks"
     
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String(10), nullable=False)
+    company_name = db.Column(db.String(128), nullable=False)
+    sector = db.Column(db.String(128), nullable=False)
+    sub_sector = db.Column(db.String(128), nullable=False)
     
+    prices = relationship("Price", back_populates="stock")
+
+class Price(db.Model):
+    """
+    Creates a table to hold stock prices. It will reference the stocks table in order to assign an
+    ID 
+
+    Args:
+        db (_type_): _description_
+    """
+    __tablename__ = "prices"
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    stock_id = db.Column(db.Integer, ForeignKey("stocks.id"), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    open_price = db.Column(db.Float, nullable=False)
+    high_price = db.Column(db.Float, nullable=False)
+    low_price = db.Column(db.Float, nullable=False)
+    close_price = db.Column(db.Float, nullable=False)
+    adjusted_close_price = db.Column(db.Float, nullable=False)
+    volume = db.Column(db.Integer, nullable=False)
+    
+    stock = relationship("Stock", back_populates="prices")
--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database.models import db
+from database.models import db, Stock, Price
 import config
 import os
 import sys
+from datetime import datetime
 
 sys.path.append(os.getcwd())
 
@@ -28,6 +29,15 @@ app.register_blueprint(signup_bp)
 
 with app.app_context():
     db.create_all()
-
+    
+    # new_stock = Stock(ticker="AAPL", company_name="Apple", sector="Tech", sub_sector="comm")
+    # db.session.add(new_stock)
+    # db.session.commit()
+    
+    # price_2023 = Price(stock_id=new_stock.id, date=datetime(2023, 7, 1), open_price=150.0, high_price=155.0, 
+    #                    low_price=149.0, close_price=152.0, adjusted_close_price=152.0, volume=1000000)
+    # db.session.add(price_2023)
+    # db.session.commit()
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
