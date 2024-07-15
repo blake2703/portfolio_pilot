93ad83dfe00ea2215ee946f1df217255883fef98
blake2703 <dickersonb27@yahoo.com>
2024-07-15 01:24:13 -0700

    Sharpe ratio metric implementation

--- backend/run.py
+++ backend/run.py
@@ -2,9 +2,13 @@ import os
 import sys
 sys.path.append(os.getcwd())
 from backend import create_app
-from wrapper.populate_db import fetch_stock_data, fetch_stock_prices
+from wrapper.metrics import get_returns, calc_statistics_sharpe
+import numpy as np
 
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        rets = get_returns()
+        print(calc_statistics_sharpe(rets))
     app.run(debug=False)
